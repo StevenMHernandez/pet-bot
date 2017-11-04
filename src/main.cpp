9e1756a408cb74ffc0d3c20f8cc9a4290c91b1de
StevenMHernandez <contact@stevenhdesigns.com>
2017-11-04 13:42:12 -0400

    added eink display library, draw images (and text) onto screen, switch board to adafruit feather huzzah, added sd card images

--- src/main.cpp
+++ src/main.cpp
@@ -3,6 +3,17 @@
 #include <ESP8266WiFi.h>
 #include "config/ConnectionParams.h"
 #include "aws/ESP8266DateTimeProvider.h"
+#include <epd.h>
+
+#define ONBOARD_LED 16
+
+#define TYPE_PIN_A 14
+#define TYPE_PIN_B 12
+#define TYPE_PIN_C 17
+
+#define VARIANT_PIN_A 16
+#define VARIANT_PIN_B 5
+#define VARIANT_PIN_C 4
 
 const char *AP_SSID = SSID_NAME;
 const char *AP_PASS = SSID_PASS;
@@ -20,15 +31,42 @@ ConnectionParams cp(sigv4);
 WebSocketClientAdapter adapter(cp);
 MqttClient client(adapter, cp);
 
-void setup() {
-    Serial.begin(115200);
-    while (!Serial) {
-        yield();
-    }
+void test_screen() {
+    epd_clear();
+    epd_disp_bitmap("DOG1.BMP", 0, 0);
+    epd_disp_string("BOW", 360, 250);
+    epd_disp_string("WOW", 360, 350);
+    epd_disp_string("WOW", 360, 450);
+    epd_udpate();
 
-    pinMode(16, OUTPUT);
-    digitalWrite(16, LOW);
+    delay(10000);
 
+    epd_clear();
+    epd_disp_bitmap("DOG0.BMP", 0, 0);
+    epd_udpate();
+}
+
+void setup_screen() {
+    epd_set_memory(MEM_TF);
+    epd_set_color(BLACK, WHITE);
+    epd_screen_rotation(EPD_INVERSION);
+    epd_set_en_font(GBK64);
+}
+
+void setup_pins() {
+    pinMode(TYPE_PIN_A, INPUT_PULLUP);
+    pinMode(TYPE_PIN_B, INPUT_PULLUP);
+    pinMode(TYPE_PIN_C, INPUT_PULLUP);
+
+    pinMode(VARIANT_PIN_A, INPUT_PULLUP);
+    pinMode(VARIANT_PIN_B, INPUT_PULLUP);
+    pinMode(VARIANT_PIN_C, INPUT_PULLUP);
+
+    pinMode(ONBOARD_LED, OUTPUT);
+    digitalWrite(ONBOARD_LED, LOW);
+}
+
+void setup_wifi_mqtt() {
     WiFi.begin(AP_SSID, AP_PASS);
     while (WiFi.status() != WL_CONNECTED) {
         delay(500);
@@ -39,18 +77,23 @@ void setup() {
     Serial.printf("mqtt connect=%d\n", res);
 
     if (res == 0) {
-        client.subscribe("t1", 1, [](const char *topic, const char *msg) {
-            Serial.printf("Got msg '%s' on topic %s\n", msg, topic);
-
-            digitalWrite(16, HIGH);
-        });
-
-        client.subscribe("t2", 0, [](const char *topic, const char *msg) {
-            Serial.printf("Got msg '%s' on topic %s\n", msg, topic);
-
-            digitalWrite(16, LOW);
+        client.subscribe("calendar", 0, [](const char *topic, const char *msg) {
+            epd_clear();
+            // epd_disp_bitmap("123.BMP", 0, 0);
+            epd_disp_string("ASCII64: Hello, World!", 0, 450);
+            epd_udpate();
         });
     }
+}
+
+void setup() {
+    Serial.begin(115200);
+
+    setup_pins();
+    setup_screen();
+    setup_wifi_mqtt();
+
+    test_screen();
 }
 
 void loop() {
